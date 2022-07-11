d086924dc0061c4d8d5bbeca48a175b6c279e63d
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-07-11 16:09:13 -0400

    back filling js and asset contents realm tests

--- host/tests/unit/realm-test.ts
+++ host/tests/unit/realm-test.ts
@@ -318,17 +318,6 @@ module('Unit | realm', function () {
   });
 
   test('realm can serve card source file', async function (assert) {
-    let cardSrc = `
-      import { contains, field, Component, Card } from 'https://cardstack.com/base/card-api';
-      import StringCard from 'https://cardstack.com/base/string';
-
-      export class Person extends Card {
-        @field firstName = contains(StringCard);
-        static isolated = class Isolated extends Component<typeof this> {
-          <template><h1><@fields.firstName/></h1></template>
-        }
-      }
-    `.trim();
     let realm = TestRealm.create({
       'dir/person.gts': cardSrc,
     });
@@ -350,17 +339,6 @@ module('Unit | realm', function () {
   });
 
   test('realm provide redirect for card source', async function (assert) {
-    let cardSrc = `
-      import { contains, field, Component, Card } from 'https://cardstack.com/base/card-api';
-      import StringCard from 'https://cardstack.com/base/string';
-
-      export class Person extends Card {
-        @field firstName = contains(StringCard);
-        static isolated = class Isolated extends Component<typeof this> {
-          <template><h1><@fields.firstName/></h1></template>
-        }
-      }
-    `.trim();
     let realm = TestRealm.create({
       'dir/person.gts': cardSrc,
     });
@@ -394,17 +372,6 @@ module('Unit | realm', function () {
   });
 
   test('realm can serve card source post request', async function (assert) {
-    let cardSrc = `
-      import { contains, field, Component, Card } from 'https://cardstack.com/base/card-api';
-      import StringCard from 'https://cardstack.com/base/string';
-
-      export class Person extends Card {
-        @field firstName = contains(StringCard);
-        static isolated = class Isolated extends Component<typeof this> {
-          <template><h1><@fields.firstName/></h1></template>
-        }
-      }
-    `.trim();
     let adapter = new TestRealmAdapter({});
     let realm = TestRealm.createWithAdapter(adapter);
     await realm.ready;
@@ -479,9 +446,102 @@ module('Unit | realm', function () {
     }
   });
 
-  skip('realm can serve compiled js file when requested without file extension ', async function () {});
+  test('realm can serve compiled js file when requested without file extension ', async function (assert) {
+    let realm = TestRealm.create({
+      'dir/person.gts': cardSrc,
+    });
+    await realm.ready;
+    let response = await realm.handle(
+      new Request('http://test-realm/dir/person')
+    );
+    assert.strictEqual(response.status, 200, 'HTTP 200 status code');
+    let compiledJS = await response.text();
+    assert.strictEqual(compiledJS, compiledCard, 'compiled card is correct');
+  });
 
-  skip('realm can serve compiled js file when requested with file extension ', async function () {});
+  test('realm can serve compiled js file when requested with file extension ', async function (assert) {
+    let realm = TestRealm.create({
+      'dir/person.gts': cardSrc,
+    });
+    await realm.ready;
+    let response = await realm.handle(
+      new Request('http://test-realm/dir/person.gts')
+    );
+    assert.strictEqual(response.status, 200, 'HTTP 200 status code');
+    let compiledJS = await response.text();
+    assert.strictEqual(compiledJS, compiledCard, 'compiled card is correct');
+  });
 
-  skip('realm can serve file asset (not card source, not js, not JSON-API)', async function () {});
+  test('realm can serve file asset (not card source, not js, not JSON-API)', async function (assert) {
+    let html = `
+      <html>
+        <body>
+          <h1>Hello World</h1>
+        </body>
+      </html>
+    `.trim();
+    let realm = TestRealm.create({
+      'dir/index.html': html,
+    });
+    await realm.ready;
+    let response = await realm.handle(
+      new Request('http://test-realm/dir/index.html')
+    );
+    assert.strictEqual(response.status, 200, 'HTTP 200 status code');
+    let responseText = await response.text();
+    assert.strictEqual(responseText, html, 'asset contents are correct');
+  });
 });
+
+const cardSrc = `
+import { contains, field, Component, Card } from 'https://cardstack.com/base/card-api';
+import StringCard from 'https://cardstack.com/base/string';
+
+export class Person extends Card {
+  @field firstName = contains(StringCard);
+  static isolated = class Isolated extends Component<typeof this> {
+    <template><h1><@fields.firstName/></h1></template>
+  }
+}
+`.trim();
+const compiledCard = `
+var _class, _descriptor, _class2;
+
+import { createTemplateFactory } from "http://externals/@ember/template-factory";
+import { setComponentTemplate } from "http://externals/@ember/component";
+
+function _initializerDefineProperty(target, property, descriptor, context) { if (!descriptor) return; Object.defineProperty(target, property, { enumerable: descriptor.enumerable, configurable: descriptor.configurable, writable: descriptor.writable, value: descriptor.initializer ? descriptor.initializer.call(context) : void 0 }); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+function _initializerWarningHelper(descriptor, context) { throw new Error('Decorating class property failed. Please ensure that ' + 'proposal-class-properties is enabled and runs after the decorators transform.'); }
+
+import { contains, field, Component, Card } from 'https://cardstack.com/base/card-api';
+import StringCard from 'https://cardstack.com/base/string';
+export let Person = (_class = (_class2 = class Person extends Card {
+  constructor(...args) {
+    super(...args);
+
+    _initializerDefineProperty(this, "firstName", _descriptor, this);
+  }
+
+}, _defineProperty(_class2, "isolated", setComponentTemplate(createTemplateFactory(
+/*
+  <h1><@fields.firstName/></h1>
+*/
+{
+  "id": null,
+  "block": "[[[10,\\"h1\\"],[12],[8,[30,1,[\\"firstName\\"]],null,null,null],[13]],[\\"@fields\\"],false,[]]",
+  "moduleName": "(unknown template module)",
+  "isStrictMode": true
+}), class Isolated extends Component {})), _class2), (_descriptor = _applyDecoratedDescriptor(_class.prototype, "firstName", [field], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function () {
+    return contains(StringCard);
+  }
+})), _class);
+`.trim();
